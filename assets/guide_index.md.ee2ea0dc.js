35400e841663ad6a6f99ad393b651de04b3adbea
Jan Müller <janmueller3698@gmail.com>
2022-07-16 16:04:47 +0000

    Deploying to gh-pages from @ DerYeger/resumon@e9c66b2cdc6177637d13378ea28df8f64c496808 🚀

---
+++ assets/guide_index.md.ee2ea0dc.js
@@ -0,0 +1,11 @@
+import{_ as a,c as s,o as n,a as e}from"./app.3f9610cb.js";const m=JSON.parse('{"title":"Guide","description":"","frontmatter":{"editLink":true,"contributors":false},"headers":[{"level":2,"title":"Installation","slug":"installation"}],"relativePath":"guide/index.md"}'),l={name:"guide/index.md"},p=e(`<h1 id="guide" tabindex="-1">Guide <a class="header-anchor" href="#guide" aria-hidden="true">#</a></h1><h2 id="installation" tabindex="-1">Installation <a class="header-anchor" href="#installation" aria-hidden="true">#</a></h2><div class="language-bash"><span class="copy"></span><pre class="vp-code-dark"><code><span class="line"><span style="color:#758575;"># yarn</span></span>
+<span class="line"><span style="color:#DBD7CA;">yarn add resumon</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#758575;"># or npm</span></span>
+<span class="line"><span style="color:#DBD7CA;">npm install resumon</span></span>
+<span class="line"></span></code></pre><pre class="vp-code-light"><code><span class="line"><span style="color:#A0ADA0;"># yarn</span></span>
+<span class="line"><span style="color:#393A34;">yarn add resumon</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A0ADA0;"># or npm</span></span>
+<span class="line"><span style="color:#393A34;">npm install resumon</span></span>
+<span class="line"></span></code></pre></div>`,3),t=[p];function o(i,r,c,d,_,u){return n(),s("div",null,t)}var y=a(l,[["render",o]]);export{m as __pageData,y as default};
